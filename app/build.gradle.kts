cab012862006c6c78117a2e907eec12d660e40d8
Eana Hufwe <eana@1a23.com>
2024-12-29 18:35:18 -0800

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "studio1a23.altTextAi"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "studio1a23.altTextAi"
         minSdk = 31
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
